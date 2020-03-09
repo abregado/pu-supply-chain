a92f5286827a29dd321bef5e0bdbbc175c32800a
Ben Buckton <contact@benbuckton.com>
2020-03-09 12:16:10 +0100

    icons step 1

--- graphics/generate_icons.py
+++ graphics/generate_icons.py
@@ -1,16 +1,25 @@
 import csv
-from PIL import Image, ImageDraw
+import math
+
+from PIL import Image, ImageDraw, ImageFont
+
+font = ImageFont.truetype('TitilliumWeb-SemiBold.ttf', 22)
 
 with open('icon-names.csv') as csvfile:
-    readCSV = csv.reader(csvfile,delimiter=',')
+    readCSV = csv.reader(csvfile, delimiter=',')
     icons = []
     for row in readCSV:
         icons.append([row[0],row[1]])
-        bgim = PIL.open("./icons/"+row[1]+".png")
+        bgim = Image.open("icon-bg/"+row[1]+".png")
         img = bgim.copy()
 
         draw = ImageDraw.Draw(img)
-        draw.text((0,0),row[0],(0,0,0,1))
+        size = draw.textsize(row[0], font)
+        print(size)
+        pos = (32 - (size[0]/2), 26 - size[1]/2)
+        draw.text(pos, row[0], (0, 0, 0, 1), font)
+
+        img.save("icons/" + str(row[0]).lower() + ".png")
 
 
 
