5601493ea6a26c4269c663a777424f6f6767ac1b
Ben Buckton <ben.buckton@factorio.com>
2020-03-09 10:33:32 +0100

    working on python icon generator

---
+++ graphics/generate_icons.py
@@ -0,0 +1,17 @@
+import csv
+from PIL import Image, ImageDraw
+
+with open('icon-names.csv') as csvfile:
+    readCSV = csv.reader(csvfile,delimiter=',')
+    icons = []
+    for row in readCSV:
+        icons.append([row[0],row[1]])
+        bgim = PIL.open("./icons/"+row[1]+".png")
+        img = bgim.copy()
+
+        draw = ImageDraw.Draw(img)
+        draw.text((0,0),row[0],(0,0,0,1))
+        
+
+
+    print(icons)
